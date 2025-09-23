cb8d72ff588a134d79bb9230068504a74e304e55
SuikaRyp <ciulbotak25@gmail.com>
2025-09-24 00:14:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+  BOT_TOKEN: "8306197685:AAF0wcHcwxesjOtFNv-T_8uzYqb2u6AeAyA", //TOKEN BOT
+  ADMIN_USERS: [7613900027], // isi user_id admin utama
+  vercelToken: "2m9YkBfQ9yVeQdCUZN8qZf32" //JANGAN DI HAPUS
+};
